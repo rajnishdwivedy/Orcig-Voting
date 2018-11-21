f22095c0dae5c51ee488a999ee1212eb5cd3cfee
rajnishdwivedy <34191500+rajnishdwivedy@users.noreply.github.com>
2018-11-20 22:51:45 -0800

    Update loginapp.py
    app.run()--Removed port information

--- loginapp.py
+++ loginapp.py
@@ -236,4 +236,5 @@ def admin():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4444)
+    #app.run(debug=True,host='0.0.0.0', port=4444)
+   app.run()
